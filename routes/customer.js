eb25d29bb09843c0645965c822755439eb65d73c
marryday <lestnecedel@gmail.com>
2020-05-20 22:55:16 +0300

    added some customer`s stuff

--- routes/customer.js
+++ routes/customer.js
@@ -5,6 +5,38 @@ const User = require("../models/users");
 const router = express.Router();
 
 
+router.get('/profile', (req, res) => {
+  let customer = {name: 'Petya', city:'Moscow'}
+  res.render('customer_profile', customer);
+})
+
+router.post('/profile/:id', async (req, res) => {
+  console.log(req.body);
+  res.redirect('/customer/profile')
+})
+
+router.get('/neworder', async (req, res) => {
+  res.render('customer_newOrder')
+})
+
+router.post('/neworder/:id', async (req, res) => {
+  class Order {
+    constructor(obj){
+      this.key = obj.key
+    }
+  }
+  
+  console.log(req.body)
+  res.redirect('/customer/neworder')
+  // const Order = new Order(req.body)
+})
+
+router.get('/myOrders', async (req, res) => {
+  
+})
+
+
+module.exports = router;
 
 
 

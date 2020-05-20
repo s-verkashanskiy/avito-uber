d0b9d25122ac34e8da004517a4f9f60280d86c42
marryday <lestnecedel@gmail.com>
2020-05-21 02:26:19 +0300

    added files uploader

--- routes/customer.js
+++ routes/customer.js
@@ -3,41 +3,66 @@ const bcrypt = require("bcrypt");
 const { sessionChecker } = require("../middleware/auth");
 const User = require("../models/users");
 const router = express.Router();
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+router.use(fileUpload({
+  debug: true, 
+}));
+// Редактирование профиля
+router.get("/profile", (req, res) => {
+  let customer = { name: "Petya", city: "Moscow" };
+  res.render("customer_profile", customer);
+});
 
-
-router.get('/profile', (req, res) => {
-  let customer = {name: 'Petya', city:'Moscow'}
-  res.render('customer_profile', customer);
-})
-
-router.post('/profile/:id', async (req, res) => {
+router.post("/profile", async (req, res) => {
   console.log(req.body);
-  res.redirect('/customer/profile')
-})
+  res.redirect("/customer/profile");
+});
+// Зарузка фотки
+router.post('/profile/upload', function(req, res) {
+  console.log(req.files)
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
 
-router.get('/neworder', async (req, res) => {
-  res.render('customer_newOrder')
-})
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+  let fileName = sampleFile.name;
 
-router.post('/neworder/:id', async (req, res) => {
+  // Use the mv() method to place the file somewhere on your server
+ sampleFile.mv('./uploads/' + fileName, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.redirect('/profile');
+  });
+});
+
+// router.post("/profile/upload", (req, res) => {
+//   if (req.files) {
+//     console.log(req.files);
+//   }
+// });
+
+// Создать новый заказ
+router.get("/neworder", async (req, res) => {
+  res.render("customer_newOrder");
+});
+
+router.post("/neworder", async (req, res) => {
   class Order {
-    constructor(obj){
-      this.key = obj.key
+    constructor(obj) {
+      this.key = obj.key;
     }
   }
-  
-  console.log(req.body)
-  res.redirect('/customer/neworder')
+
+  console.log(req.body);
+  res.redirect("/customer/neworder");
   // const Order = new Order(req.body)
-})
+});
 
-router.get('/myOrders', async (req, res) => {
-  
-})
-
+router.get("/myOrders", async (req, res) => {});
 
 module.exports = router;
-
-
 
 module.exports = router;
